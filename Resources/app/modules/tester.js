fc01f3337375f88219bbfb596f68493c9a53a3f2
Kris <kris@aliencom.net>
2013-04-11 01:44:40 -0400

    added less compiler tests

--- Resources/app/modules/tester.js
+++ Resources/app/modules/tester.js
@@ -4,7 +4,8 @@
  * This module handles the observed .less files and starts the compilation process, if needed.
  */
 define(['modules/compiler',
-        'text!templates/tester-modal.html'], function (compiler, tmp_src_modalHtml) {
+        'modules/file_model',
+        'text!templates/tester-modal.html'], function (compiler, File_Model, tmp_src_modalHtml) {
 
     console.log('to run tests, write : startTests() in the console.');
 
@@ -34,6 +35,61 @@ define(['modules/compiler',
                 ok( true, "Passed and ready to resume!" );
                 start();
             }, 1000);
+        });
+
+
+        module('Compiler-Tests');
+
+        //TODO : we will want to loop through the subdirectories so we can run many tests
+
+
+        asyncTest( "1-simple-vars", function() {
+            var inputFile = Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDirectory(),'/Resources/tests/compiler-tests/1-simple-vars/input.less');
+            var fileModel = new File_Model({
+                input_file:inputFile,
+                skipStorage:true
+            });
+
+            var outputFile = Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDirectory(),'/Resources/tests/compiler-tests/1-simple-vars/output.css');
+            var expectedCss = outputFile.open().read().toString();
+
+            compiler.compile_to_string(fileModel,function(model,css){
+                var passed = (css === expectedCss);
+
+                if(!passed){
+                    //we want to dump the vars
+                    console.log('compiled',css);
+                    console.log('expected',expectedCss);
+                }
+                ok( passed, "Compiled LESS matches expected output" );
+                start();
+            });
+        });
+
+
+        asyncTest( "2-paths", function() {
+            var inputFile = Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDirectory(),'/Resources/tests/compiler-tests/2-paths/input.less');
+            var fileModel = new File_Model({
+                input_file:inputFile,
+                skipStorage:true
+            });
+
+            var outputFile = Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDirectory(),'/Resources/tests/compiler-tests/2-paths/output.css');
+            var expectedCss = outputFile.open().read().toString();
+
+            compiler.compile_to_string(fileModel,function(model,css){
+                var passed = (css === expectedCss);
+
+                if(!passed){
+                    //we want to dump the vars
+                    console.log('compiled:');
+                    console.log(css)
+                    console.log('expected:');
+                    console.log(expectedCss)
+                }
+                ok( passed, "Compiled LESS matches expected output" );
+                start();
+            });
         });
 
 
