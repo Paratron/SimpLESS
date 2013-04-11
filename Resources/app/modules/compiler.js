7d5d858516435d87b9780c8251634e22952d10ca
Kris <kris@aliencom.net>
2013-04-10 23:56:49 -0400

    refactored compiler for testing

--- Resources/app/modules/compiler.js
+++ Resources/app/modules/compiler.js
@@ -40,101 +40,114 @@ define(['less', '3p/cleancss'], function (less, cleancss) {
         return css;
     }
 
-    var obj = {
-        current_model:null,
-        compile:function (model) {
-            var model_dta = model.toJSON();
+    function prefix(model,callBack){
 
-            model.set({
-                last_compilation:(new Date()).getTime() * 1000,
-                error:null
-            });
-
-            obj.current_model = model;
-
-            var lesscode = model_dta.input_file.open().read().toString();
-            
-            
-            var parserConfig = {
-              // Specify search paths for @import directives
-              'paths' : [
-                model_dta.input_file.parent().toURL() + '/' //folder toURL requires trailing slash
-              ],
-                filename: model_dta.input_file.toURL()  // Specify a filename, for better error messages
-            };
-
-            // alittle ugly because lesscss 3.31 doesn't passthrough debug to imports.
-            //thus the need for the static less.dumpLineNumbers
-            if(model_dta.settings.debugLines){
-              less.dumpLineNumbers = true; 
-              parserConfig.dumpLineNumbers = 'all';
-            }else{
-              less.dumpLineNumbers = false;
-            }
-            var parser = new less.Parser(parserConfig);
-
-            try {
-                parser.parse(lesscode, function (err, tree) {
-                    if (err) {
-                        model.set({
-                            error:err
-                        });
-                        model.trigger('compilation:error', err);
-                        model.collection.trigger('compilation:error', err);
-                        return;
-                    }
-
-                    var csscode = tree.toCSS(parserConfig);
-
-                    csscode = postCreateCleanup(csscode);
-
-                    var comment_regex = /^\/\*([^\*\/]+?)\*\//gm;
-                    var result = comment_regex.exec(csscode);
-                    if (result) {
-                        beginning_comment = result[0];
-                    } else {
-                        beginning_comment = '';
-                    }
-
-                    if (model_dta.settings.minify) {
-                        csscode = cleancss.process(csscode);
-                    }
-
-                    if (model_dta.settings.prefix) {
-                        model.set({
-                            uploading:true
-                        });
-                        var ajax_data = {
-                            css:csscode,
-                            simpless:settings.version
-                        }
-                        if (model_dta.settings.minify) {
-                            ajax_data.compress_option = 'on';
-                        }
-                        $.ajax({
-                            async:true,
-                            cache:false,
-                            data:ajax_data,
-                            type:'POST',
-                            url:'http://prefixr.com/api/index.php?' + Math.random(),
-                            success:function (response) {
-                                model.set({
-                                    uploading:false
-                                });
-                                save_css_file(model, response);
-                            }
-                        });
-
-                    } else {
-                        //Do not prefix.
-                        save_css_file(model, csscode);
-                    }
-                });
-            }
-            catch (e) {
-                console.log(e);
-            }
+        model.set({
+            uploading:true
+        });
+        var ajax_data = {
+            css:csscode,
+            simpless:settings.version
         }
+        if (model_dta.settings.minify) {
+            ajax_data.compress_option = 'on';
+        }
+        $.ajax({
+            async:true,
+            cache:false,
+            data:ajax_data,
+            type:'POST',
+            url:'http://prefixr.com/api/index.php?' + Math.random(),
+            success:function (response) {
+                model.set({
+                    uploading:false
+                });
+                if(callBack) callBack(model,response);
+            }
+        });
+    }
+
+    function compile(model){
+        compile_to_string(model,function(model,csscode){
+            save_css_file(model,csscode);
+        });
+    }
+
+    function compile_to_string(model,callBack){
+        var model_dta = model.toJSON();
+
+        model.set({
+            last_compilation:(new Date()).getTime() * 1000,
+            error:null
+        });
+
+        var lesscode = model_dta.input_file.open().read().toString();
+
+
+        var parserConfig = {
+            // Specify search paths for @import directives
+            'paths' : [
+                model_dta.input_file.parent().toURL() + '/' //folder toURL requires trailing slash
+            ],
+            filename: model_dta.input_file.toURL()  // Specify a filename, for better error messages
+        };
+
+        // alittle ugly because lesscss 3.31 doesn't passthrough debug to imports.
+        //thus the need for the static less.dumpLineNumbers
+        if(model_dta.settings.debugLines){
+            less.dumpLineNumbers = true;
+            parserConfig.dumpLineNumbers = 'all';
+        }else{
+            less.dumpLineNumbers = false;
+        }
+        var parser = new less.Parser(parserConfig);
+
+        try {
+            parser.parse(lesscode, function (err, tree) {
+                if (err) {
+                    model.set({
+                        error:err
+                    });
+                    model.trigger('compilation:error', err);
+                    model.collection.trigger('compilation:error', err);
+                    return;
+                }
+
+                var csscode = tree.toCSS(parserConfig);
+
+                csscode = postCreateCleanup(csscode);
+
+                var comment_regex = /^\/\*([^\*\/]+?)\*\//gm;
+                var result = comment_regex.exec(csscode);
+                if (result) {
+                    beginning_comment = result[0];
+                } else {
+                    beginning_comment = '';
+                }
+
+                if (model_dta.settings.minify) {
+                    csscode = cleancss.process(csscode);
+                }
+
+                if (model_dta.settings.prefix) {
+                    prefix(model, function(model,csscode){
+                        callBack(model, csscode);
+                    });
+                } else {
+                    //Do not prefix.
+                    callBack(model, csscode);
+                }
+            });
+        }
+        catch (e) {
+            console.log(e);
+        }
+
+    }
+
+    var obj = {
+        compile:compile,
+        compile_to_string:compile_to_string
     }
 
     _.extend(obj, Backbone.Events);
