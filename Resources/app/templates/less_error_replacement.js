1d23d0a94d347342adbf8abfcaf951606cae9f6d
Christian Engel <christian.engel@wearekiss.com>
2013-10-22 20:55:54 +0200

    Purged old codebase

--- Resources/app/templates/less_error_replacement.js
+++
@@ -1,5 +0,0 @@
-function error(e, href) {
-        window.require(['modules/compiler'], function (compiler) {
-            compiler.current_model.trigger('compilation:error', e);
-        });
-    }
