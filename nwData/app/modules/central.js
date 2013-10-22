c97e57f6db85237b4d07b84c08770ed16b3b4afb
Christian Engel <christian.engel@wearekiss.com>
2013-10-22 22:12:05 +0200

    Added file watchers for imported less files

--- nwData/app/modules/central.js
+++ nwData/app/modules/central.js
@@ -10,6 +10,8 @@ define([], function (){
         nodeLESS,
         pathSeparator;
 
+    Backbone.sync = function(){};
+
     nodeFS = nodeRequire('fs');
     nodeLESS = nodeRequire('less');
 
@@ -23,6 +25,50 @@ define([], function (){
     _.extend(central, Backbone.Events);
 
     /**
+     * A file watcher element that automatically triggers compilation on the model that
+     * has created the element.
+     */
+    central._models.FileWatcher = Backbone.Model.extend({
+        defaults: {
+            fileName: '',
+            listModel: null
+        },
+        idAttribute: 'fileName',
+        initialize: function (){
+            var that;
+
+            that = this;
+
+            if(!nodeFS.existsSync(this.get('fileName'))){
+                this.destroy();
+                return;
+            }
+
+            this.on('destroy', function (){
+                console.log('Not watching ' + that.get('fileName') + ' anymore...');
+                this.watcher.close();
+            });
+
+            this.watcher = nodeFS.watch(this.get('fileName'), function (event){
+                if(event === 'change'){
+                    that.get('listModel').compile();
+                    return;
+                }
+
+                that.destroy();
+            });
+
+            console.log('Now watching ' + this.get('fileName'));
+        }
+    });
+
+    central._collections.FileWatcherCollection = Backbone.Collection.extend({
+        model: central._models.FileWatcher
+    });
+
+    //===================================================================
+
+    /**
      * Default Model to keep watching on LESS files.
      * @type {*}
      */
@@ -33,11 +79,11 @@ define([], function (){
             outputPath: '',
             fileName: '',
             lastChange: 0,
-            includes: 0,
             doPrefix: false,
             doMinify: true,
             doLove: true,
-            state: 0
+            state: 0,
+            fileWatchers: null
         },
         initialize: function (){
             var path,
@@ -46,10 +92,12 @@ define([], function (){
             path = this.attributes.inputPath.split('/');
             that = this;
 
+            this.attributes.fileWatchers = new central._collections.FileWatcherCollection();
+
             this.attributes.fileName = path.pop();
             this.attributes.inputFolder = path.join('/') + '/';
 
-            this.on('change:outputPath', function(){
+            this.on('change:outputPath', function (){
                 this.set('outputFileName', this.get('outputPath').split('/').pop());
             });
 
@@ -71,30 +119,45 @@ define([], function (){
 
             this.getFileMTime();
 
-            this.watcher = nodeFS.watch(this.attributes.inputPath, function (event){
-                if(event === 'change'){
-                    that.compile();
-                    that.getFileMTime();
-                    return;
-                }
-
-                if(event === 'rename'){
-                    var name;
-                    that.stopListening();
-                    name = that.get('fileName');
-                    central.observedFiles.remove(that);
-                    alert('Stopped watching "' + name + '" because it has been renamed or deleted.');
-                }
+            //Watch the Input File!
+            this.attributes.fileWatchers.add({
+                fileName: this.get('inputPath'),
+                listModel: that
             });
+
+            console.log('Imports: ', this.getImportedPaths(this.get('inputPath')));
         },
         /**
          * Compiles the LESS source to CSS.
          */
         compile: function (){
             var that,
-                parser;
+                parser,
+                i,
+                importedFiles;
 
             that = this;
+
+            //Update the List of file watchers.
+            importedFiles = this.getImportedPaths(this.get('inputPath'), true);
+
+            //First, try to add all files again. Already watched files will be ignored.
+            for(i = 0; i < importedFiles.length; i++){
+                this.get('fileWatchers').add({
+                    fileName: importedFiles[i],
+                    listModel: that
+                });
+            }
+
+            //Now, remove all watchers of files, no longer being imported.
+            this.get('fileWatchers').each(function(watcher){
+                if(watcher.get('fileName') === that.get('inputPath')){
+                    return;
+                }
+                if(importedFiles.indexOf(watcher.get('fileName')) === -1){
+                    watcher.destroy();
+                }
+            });
 
             nodeFS.readFile(that.get('inputPath'), {encoding: 'utf8'}, function (err, data){
                 parser = new (nodeLESS.Parser)({
@@ -128,6 +191,60 @@ define([], function (){
             });
         },
         /**
+         * This method will scan the less file (and included less files) for @import
+         * statements and will fetch a list of paths to all less files to be watched.
+         */
+        getImportedPaths: function (filename, justFind){
+            var that,
+                i,
+                fs,
+                path,
+                regex,
+                fileContent,
+                result,
+                imports,
+                importFile,
+                filePath;
+
+            that = this;
+            fs = nodeRequire('fs');
+            path = nodeRequire('path');
+
+            regex = /^[^/]*@import.+?["'](.+?)["']/g;
+            imports = [];
+            filePath = path.dirname(filename);
+
+            fileContent = fs.readFileSync(filename, {encoding: 'utf8'});
+
+            console.log('Searching for @imports in ' + filename);
+
+            while (result = regex.exec(fileContent)) {
+                importFile = path.resolve(filePath, result[1]);
+                if(importFile === true){
+                    continue;
+                }
+                if(fs.existsSync(importFile)){
+                    imports.push(importFile);
+                    imports = imports.concat(that.getImportedPaths(importFile, true));
+                }
+            }
+
+            console.log(imports.length + ' @imports found');
+
+            if(!justFind){
+                console.log('Creating watchers for found files...');
+
+                for (i = 0; i < imports.length; i++) {
+                    that.get('fileWatchers').add({
+                        fileName: imports[i],
+                        listModel: that
+                    });
+                }
+            }
+
+            return imports;
+        },
+        /**
          * Gets the current input file change time.
          */
         getFileMTime: function (){
@@ -145,7 +262,9 @@ define([], function (){
          * Stops the file watcher before the object is being destroyed.
          */
         stopWatching: function (){
-            this.watcher.close();
+            this.get('fileWatchers').each(function(watcher){
+                watcher.destroy();
+            });
         }
     });
 
